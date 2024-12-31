0a2305f8321d597fcb43a8ba2c4e77d396689214
Kyoya67 <jackma8569@gmail.com>
2024-12-31 23:34:36 +0900

    Fix Modal Error

--- src/app/_components/Modal.tsx
+++ src/app/_components/Modal.tsx
@@ -19,7 +19,7 @@ export default function Modal() {
     }, [isMenuOpen]);
 
     return (
-        <div>
+        <div className="overflow-hidden">
             <Image
                 src="/webP_mobile/menu.webp"
                 alt="メニュー"
@@ -27,12 +27,20 @@ export default function Modal() {
                 height={100}
                 style={{ zIndex: 1 }}
                 objectFit="cover"
-                className="absolute top-0 right-0 mt-3 mr-3 cursor-pointer"
+                className="absolute top-0 right-0 mt-4 mr-3 cursor-pointer"
                 onClick={toggleMenu}
             />
             <div
-                className={`absolute top-0 right-0 bg-[#f5f8f9] shadow-lg p-4 z-10 w-3/5 h-full transition-transform duration-300 ${isMenuOpen ? "translate-x-0" : "translate-x-full"
-                    }`}
+                className={`
+                    fixed top-0 right-0 
+                    bg-[#f5f8f9] 
+                    shadow-lg 
+                    p-4 z-10 w-3/5 h-full 
+                    transition-transform 
+                    duration-300 
+                    transform ${isMenuOpen ? "translate-x-0" : "translate-x-full"}
+                `}
+                style={{ transform: isMenuOpen ? "translateX(0)" : "translateX(100%)" }}
             >
                 <button onClick={toggleMenu} className="absolute top-2 right-2">
                     <Image
@@ -44,6 +52,12 @@ export default function Modal() {
                     />
                     <span className="text-[#000f9f] absolute mt-[-0.5rem] right-2.5">Close</span>
                 </button>
+
+                <div className="text-[#000f9f] text-left text-lg mt-20">
+                    情報科学芸術大学院大学<br></br>
+                    第23期生修了研究発表会・<br></br>
+                    プロジェクト研究発表会<br></br>
+                </div>
                 <ul>
                     <li>メニュー項目1</li>
                     <li>メニュー項目2</li>
