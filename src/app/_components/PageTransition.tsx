d5559240d426924d6ee8adba997187b497c84dfb
Kyoya67 <jackma8569@gmail.com>
2025-01-01 13:10:11 +0900

    Change transition duration

--- src/app/_components/PageTransition.tsx
+++ src/app/_components/PageTransition.tsx
@@ -12,7 +12,7 @@ export default function PageTransition({ children }: { children: React.ReactNode
                 key={pathname}
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
-                transition={{ duration: 0.3 }}
+                transition={{ duration: 0.5 }}
             >
                 {children}
             </motion.div>
