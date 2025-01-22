c76a0480bd289c8392d5154871230f84af983250
Kyoya67 <jackma8569@gmail.com>
2025-01-22 19:36:22 +0900

    fix modal font-size mobile

--- src/app/_components/master/StudentContent.tsx
+++ src/app/_components/master/StudentContent.tsx
@@ -75,7 +75,7 @@ export function StudentContent({
                         <LocalizedTextClient
                             ja={workTitleJapanese}
                             en={workTitleEnglish}
-                            className="text-fluid-sm mb-2"
+                            className="text-fluid-base mb-2 text-[#000f9f]"
                         />
                         <div className="mb-4 relative aspect-video w-full">
                             <Image
