ac9629de39d2339503155bff69d4b15baec2e955
Kyoya67 <jackma8569@gmail.com>
2025-01-23 19:27:09 +0900

    fix error ts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,8 @@ export const metadata: Metadata = {
     width: 'device-width',
     initialScale: 1,
     maximumScale: 1,
-    userScalable: 'no'
+    minimumScale: 1,
+    userScalable: false
   }
 };
 
